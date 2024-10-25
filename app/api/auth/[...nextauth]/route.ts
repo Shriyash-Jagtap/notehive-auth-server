e1399234f8ee53350cebedb2e0f61917503e987e
Shriyash Jagtap <jagtapshriyash4@gmail.com>
2024-10-25 11:52:58 +0530

    fixed ESLint

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { NextRequest } from "next/server";
 
 // Define NextAuth options without exporting them
 const authOptions = {

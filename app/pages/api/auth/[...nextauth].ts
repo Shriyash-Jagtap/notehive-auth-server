8ddc3c2ea6934d0ad8bcff2e7738fbd64332e0e6
Shriyash Jagtap <jagtapshriyash4@gmail.com>
2024-10-25 11:31:57 +0530

    Initial commit for auth server

---
+++ app/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+// ./pages/api/auth/[...nextauth].ts
+
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  // Add other NextAuth options as needed
+};
+
+export default NextAuth(authOptions);
